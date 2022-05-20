4182da8f0431d4abb1dfef2045952c3665f12bd6
BBiroo <yeojining5@gmail.com>
2022-05-21 02:46:29 +0900

    백준 문제 풀이

---
+++ JavaStudy/src/BaekJoon/Main10926.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Main10926 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		
+		System.out.println(input+"??!");
+	}
+}
+// String input = sc.next(); // next()는 공백 전까지만 출력함
+// yeojin ing 입력 시 > yeojin만 출력함
